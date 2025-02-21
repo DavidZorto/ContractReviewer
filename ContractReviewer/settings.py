8e040c2e0b5c2ae281517d5b724093f9d0b67210
DavidZorto <79228681+DavidZorto@users.noreply.github.com>
2025-02-21 01:42:47 +0000

    Connected to Render DB

--- ContractReviewer/settings.py
+++ ContractReviewer/settings.py
@@ -155,3 +155,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_HTTPONLY = True
+
+AUTH_USER_MODEL = 'Kinde.CustomUser'
--- Kinde/models.py
+++ Kinde/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    kinde_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
