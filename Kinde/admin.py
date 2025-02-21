b523369ce613985e3db21c410c3c860d3524799e
DavidZorto <79228681+DavidZorto@users.noreply.github.com>
2025-02-21 02:44:54 +0000

    Admin Backend

--- Kinde/admin.py
+++ Kinde/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_superuser', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+# If there are other models, register them too
